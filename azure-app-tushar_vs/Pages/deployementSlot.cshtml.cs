db61bb3ec5803c584f3a8c377f7878d318d154ac
tushar <sachindip2010@gmail.com>
2025-11-16 19:26:56 +0530

    added changes for staging

---
+++ azure-app-tushar_vs/Pages/deployementSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_tushar_vs.Pages
+{
+    public class deployementSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
